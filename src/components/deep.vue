e01b54b4da6e6ed1970dfbb60ff96b356436a2a7
crb <1811709098@qq.com>
2019-10-12 17:44:48 +0800

    ddscds

---
+++ src/components/deep.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-button type="primary">主要按钮</el-button>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .el-button--primary{
+    background-color: red;
+  }
+</style>
---
+++ src/components/import-js.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    nhgnhghn
+  </div>
+</template>
+
+<script>
+import jd from './js.js'
+export default {
+    name: 'loading',
+    computed: {
+    
+    },
+    data () {
+        return {
+          show: false
+      };
+    },
+    methods: {
+      aa() {
+        import('./js.js').then(data =>{
+          console.log(jd)
+        })
+      }
+    },
+    created () {
+      this.aa()
+    }
+};
+</script>
+
+
+<style>
+
+</style>
